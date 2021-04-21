6ff9e0d1b78192845246f931c59e4ff47d8b37ee
kotkotei <kotkotei@yandex.ru>
2021-04-21 12:20:24 +0700

    jregitjghtjhet

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введи сил знач");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Понедельник");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("вторник");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("среда");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("четверг");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("пятница");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("субта");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Воскрнесенье");
+                        break;
+                    }
+            }
+        }
+    }
+}
+
